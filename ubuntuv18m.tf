586a9a01ad28dec17de84788485a1871c970b7df
Constantine-ZU <ktb747@gmail.com>
2024-06-01 22:42:10 +0300

    22

--- ubuntuv18m.tf
+++ ubuntuv18m.tf
@@ -45,5 +45,54 @@ resource "azurerm_linux_virtual_machine" "vm_20_7" {
     username   = "ubuntu"
     public_key = var.ssh_public_key  
   }
+ # Provisioners
+  provisioner "file" {
+    source      = "setup_instance.sh"
+    destination = "/tmp/setup_instance.sh"
+    connection {
+      type        = "ssh"
+      host        = self.public_ip_address
+      user        = "ubuntu"
+      private_key = file(var.ssh_private_key)
+    }
+  }
+
+  provisioner "file" {
+    source      = "restore_pg_dump.sh"
+    destination = "/tmp/restore_pg_dump.sh"
+    connection {
+      type        = "ssh"
+      host        = self.public_ip_address
+      user        = "ubuntu"
+      private_key = file(var.ssh_private_key)
+    }
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/setup_instance.sh /usr/local/bin/setup_instance.sh",
+      "sudo chmod +x /usr/local/bin/setup_instance.sh"
+    #   ,"export S3_PATH='s3://constantine-z-2/'",
+    #   "export PFX_FILE_NAME='webaws_pam4_com_2024_05_13.pfx'",
+    #   "export APP_NAME='BlazorAut'",
+    #   "export S3_BASE_URL='https://constantine-z.s3.eu-north-1.amazonaws.com'",
+    #   "export DB_HOST='pgaws.pam4.com'",
+    #   "export DB_USER='dbuser'",
+    #   "DB_PASS=$(cat /tmp/db_password)",
+    #   "export DB_PASS",
+    #   "export DB_NAME='dbwebaws'"
+      ,"sudo mv /tmp/restore_pg_dump.sh /usr/local/bin/restore_pg_dump.sh"
+      ,"sudo chmod +x /usr/local/bin/restore_pg_dump.sh"
+    #   ,"sudo -E /usr/local/bin/restore_pg_dump.sh"
+    #   ,"sudo -E /usr/local/bin/setup_instance.sh"
+    ]
+    connection {
+      type        = "ssh"
+      host        = self.public_ip_address
+      user        = "ubuntu"
+      private_key = file(var.ssh_private_key)
+    }
+  }
+
+
   depends_on = [azurerm_postgresql_flexible_server.pg_db]
 }
